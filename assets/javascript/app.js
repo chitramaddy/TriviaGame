c23f561aa0210749c03f9b0455338473aea11540
Your Name <you@example.com>
2018-03-05 11:39:50 -0500

    has issues

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -180,7 +180,7 @@ var questionsArr = [{
 ];
 
 //var for incrementing question number
-var count = 0;
+var x = 0;
 
 //var for storing player answer
 var ansChosen = '';
@@ -192,7 +192,7 @@ var gameIsRunning = false;
 var displayQues;
 
 //variable to count correct answers
-var correctAns = 0; 
+var correctAns = 0;
 
 //variable to count incorrect answers
 var incorrectAns = 0;
@@ -217,27 +217,44 @@ var correctHTML = document.getElementById('correct');
 //DOM reference to incorrect answers
 var incorrectHTML = document.getElementById('incorrect');
 
+
+
+
+$(document).ready(function () {
 //When document is ready
+$(".big").on("click", function () {
+
+    //hide play button
+    $(".big").hide(this);
+
+    //when play is clicked: rewrite stats, pick a question, set game running to true
+    correctAns = 0;
+    incorrectAns = 0;
+    gameIsRunning = true;
+    console.log(count);
 
 
-//show play button
-
-//when play is clicked: rewrite stats, pick a question, set game running to true
-//$("#play").on("click", startTrivia);
-
-//write picked question and respective answer choices to the DOM, set the timer running write the timer to DOM
-$("#ques").html(questionsArr[count].Q);
+    //write picked question and respective answer choices to the DOM, set the timer running write the timer to DOM
+    $("#ques").text(questionsArr[count].Q);
 console.log(questionsArr[count].Q);
 
-$("#A1").html(questionsArr[count].A1);
-$("#A2").html(questionsArr[count].A2);
-$("#A3").html(questionsArr[count].A3);
-$("#A4").html(questionsArr[count].A4);
-//if the player did not answer the question with in 10 sec, show timeout and game running false
-//store the player anwer to a variable, stop the timer, set game running to false
-//check is player answer is same as the correct answer
-//if yes, show correct answer message.increment correct answer variable, write it to the DOM
-//if not, show the incorrect answer message with correct answer. increment incorrect answer variable and write it to the DOM
-//set the time interval to 1 sec
-//show the next question
-//At the end of the array, show the stats, set game running to false, empty div and show the play button.
+$("#A1").text(questionsArr[count].A1);
+$("#A2").text(questionsArr[count].A2);
+$("#A3").text(questionsArr[count].A3);
+$("#A4").text(questionsArr[count].A4);
+
+    displayQues = setInterval(nextQues, 1000 * 10);
+
+    //if the player did not answer the question with in 10 sec, show timeout and game running false
+    setTimeout(checkAns, 1000);
+
+    //store the player anwer to a variable, stop the timer, set game running to false
+   
+    //check is player answer is same as the correct answer
+    //if yes, show correct answer message.increment correct answer variable, write it to the DOM
+    //if not, show the incorrect answer message with correct answer. increment incorrect answer variable and write it to the DOM
+    //set the time interval to 1 sec
+    //show the next question
+    //At the end of the array, show the stats, set game running to false, empty div and show the play button.
+});
+});
